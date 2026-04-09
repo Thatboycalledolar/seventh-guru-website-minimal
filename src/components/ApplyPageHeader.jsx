cbed6184528d2780d87a1bc0d82decee1e2412da
olar <kaffoolar@gmail.com>
2026-04-09 19:02:31 +0100

    application redirect page

--- src/components/ApplyPageHeader.jsx
+++ src/components/ApplyPageHeader.jsx
@@ -2,10 +2,10 @@
 
 export default function ApplyPageHeader() {
   return(
-    <div className="bg-(--primary-color) w-full pt-36 md:pt-44 pb-20 md:pb-24">
-      <div className="flex flex-col items-center md:items-start max-w-300 mx-auto px-8">
-        <h1 className="text-white text-5xl font-bold text-center md:text-start md:text-7xl mb-4">Apply <span className="text-(--secondary-color)">to work with us</span> </h1>
-        <p className="opacity-70 text-white font-body text-sm md:text-lg leading-5 text-center md:text-start md:max-w-[80%]">Tell us about your project and we'll see if we're a good fit.</p>
+   <div className="bg-gray-100 w-full pt-36 md:pt-44 pb-20 md:pb-24">
+      <div className="flex flex-col items-center max-w-300 mx-auto px-8">
+        <h1 className="text-(--text-color) text-5xl tracking-tighter max-w-[80%] font-bold text-center md:text-6xl mb-4 md:mb-8">Apply to work <br /><span className="text-(--primary-color)"> with Us</span> </h1>
+        <p className="opacity-70 text-(--text-color) font-body text-sm md:text-base leading-5 text-center max-w-[80%] md:max-w-[70%]">Tell us about your project and we'll see if we're a good fit.</p>
       </div>
     </div>
   )
--- src/pages/ApplicationRecieved.jsx
+++ src/pages/ApplicationRecieved.jsx
@@ -6,27 +6,30 @@ import { Link } from "react-router-dom"
 
 export default function ApplicationRecieved() {
   return(
-    <div className="flex flex-col items-center w-full px-8 pt-36">
-      <div className="flex flex-col items-center gap-6 mb-18 text-(--text-color) mt-20" >
-        <h1 className="text-4xl text-center font-bold">Application Submitted <br />Successfully</h1>
-        <CircleCheck color="#0059AA" width={96} height={96} />
-      </div>
+    <div className="flex flex-col items-center w-full ">
+      <div className=" bg-gray-100 w-full px-8 pt-36 mb-30">
+        <div className="max-w-300 mx-auto flex flex-col items-center">
+            <div className="flex flex-col items-center gap-6 mb-18 text-(--text-color) mt-20" >
+              <h1 className="text-3xl md:text-4xl text-center font-bold">Application Submitted <br />Successfully</h1>
+              <CircleCheck color="#0059AA" width={96} height={96} />
+            </div>
 
-      
-        <p className="bg-(--background-color) py-8 px-6 text-sm md:text-base rounded-lg gap-2  text-center mb-18">
-          Our Onboarding team is evaluating your project and will back to you within 24hrs 
-        </p>
+            
+              <p className="bg-gray-200 py-6 px-6 text-sm md:text-base rounded-lg gap-2  text-center md:w-[60%] mb-18">
+                Our Onboarding team is evaluating your project and will back to you within 24hrs 
+              </p>
 
 
-      <div className="flex flex-col items-center gap-4 mb-36">
-        <h3>Meanwhile</h3>
-        <div className="flex flex-col items-center gap-4">
-          <MainBtn text="View some Case Studies" link="/CaseStudy"/>
-          <Link to="/">
-            <button className="bg-red-500 px-4 py-3 text-sm rounded-sm text-white cursor-pointer hover:bg-red-700">Back to Homepage</button>
-          </Link>
+            <div className="flex flex-col items-center gap-4 mb-36">
+              <h3>Meanwhile</h3>
+              <div className="flex flex-col items-center gap-4">
+                <MainBtn text="View some Case Studies" link="/CaseStudy"/>
+                <Link to="/">
+                  <button className="bg-red-500 px-5 py-3 text-sm rounded-full text-white cursor-pointer hover:bg-red-700">Back to Homepage</button>
+                </Link>
+              </div>
+          </div>
         </div>
-        
       </div>
 
       <Contact />
