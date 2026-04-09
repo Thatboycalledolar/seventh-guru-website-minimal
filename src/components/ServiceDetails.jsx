fea927f2fd09d7a29dc96b0d40e38c495ba6fb5a
olar <kaffoolar@gmail.com>
2026-04-09 16:45:05 +0100

    service card font fix

--- src/components/ServiceDetails.jsx
+++ src/components/ServiceDetails.jsx
@@ -4,7 +4,7 @@ export default function ServiceDetails({ header, body}) {
   return(
     <div className="flex flex-col gap-2 md-gap-4">
           <h2 className="text-4xl md:text-5xl font-bold text-(--text-color)">{header}</h2>
-          <p className="text-sm md:text-lg">{body}</p>
+          <p className="text-sm md:text-lg leading-6 font-light">{body}</p>
     </div>
   )
 }
